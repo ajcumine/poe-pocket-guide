66b0f4ef20bccf08a83f549f30a64be3f890ff5c
ajcumine <ajcumine@gmail.com>
2021-01-19 11:53:25 +0000

    next app setup with ts and linting

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import React from 'react';
+
+import { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
