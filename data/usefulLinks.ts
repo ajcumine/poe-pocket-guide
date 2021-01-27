4b7200021e352e0144978e946a936322fedb9067
ajcumine <ajcumine@gmail.com>
2021-01-27 16:07:06 +0000

    useful links data table

---
+++ data/usefulLinks.ts
@@ -0,0 +1,15 @@
+import { MenuItems } from './types';
+
+const usefulLinks: MenuItems = {
+  1: { name: 'Offical PoE Trade', href: 'https://www.pathofexile.com/trade' },
+  2: { name: 'PoE Ninja', href: 'https://poe.ninja/' },
+  3: { name: 'Craft of Exile', href: 'https://www.craftofexile.com/' },
+  4: { name: 'PoEDB', href: 'https://poedb.tw/' },
+  5: { name: 'PoE Lab', href: 'https://www.poelab.com/' },
+  6: { name: 'Fossilizer', href: 'https://fossilizer.gitlab.io/' },
+  7: { name: 'Vorici Calculator', href: 'https://siveran.github.io/calc.html' },
+  8: { name: 'Filterblade', href: 'https://filterblade.xyz/' },
+  9: { name: 'PoE Vault', href: 'https://www.poe-vault.com/' },
+};
+
+export default usefulLinks;
